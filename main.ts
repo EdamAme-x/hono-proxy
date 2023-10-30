4bb1c986e13bbf6def3ccd8b8a19bc491ad6c8d1
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-10-30 06:04:22 +0000

    add

--- main.ts
+++ main.ts
@@ -6,12 +6,20 @@ app.get("/*", async (c) => {
     const url = c.req.path.substring(1, c.req.path.length);
     const headers = c.req.headers;
 
+    let sendURL = "";
+
+    try {
+        sendURL = new URL(url).origin;
+    }catch(_e) {
+        return new Response("Required URL")
+    }
+
     const sendHeaders = new Headers(headers)
     // mask
     sendHeaders.delete("x-real-ip")
     sendHeaders.delete("x-forwarded-for")
 
-    const resp = await fetch(url, sendHeaders as any);
+    const resp = await fetch(sendURL, sendHeaders as any);
 
     return resp;
 });
